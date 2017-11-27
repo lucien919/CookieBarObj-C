76880346cbf1891d0e927801648274e7f42bb017
Mac <mac@MacBook-Pro.local>
2017-11-27 11:10:40 -0500

    Initial Commit

---
+++ CookieBarObj-CTests/CookieBarObj_CTests.m
@@ -0,0 +1,39 @@
+//
+//  CookieBarObj_CTests.m
+//  CookieBarObj-CTests
+//
+//  Created by Mac on 11/27/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CookieBarObj_CTests : XCTestCase
+
+@end
+
+@implementation CookieBarObj_CTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
